44a14306ee5c6487bd927e5c39c38bbdf08d69ee
maksuedka <mfalejchik07@gmail.com>
2026-06-10 17:52:46 +0300

    Add profiles local/production

--- target/generated-sources/annotations/by/foxclub/mapper/ClubMapperImpl.java
+++ target/generated-sources/annotations/by/foxclub/mapper/ClubMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-05-29T02:02:51+0300",
+    date = "2026-06-10T17:38:20+0300",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.5 (BellSoft)"
 )
 @Component
